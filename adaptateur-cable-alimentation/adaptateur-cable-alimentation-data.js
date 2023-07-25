ed2d02be3049fd50f9fb9fa26bc0e561aee27644
RADJA-Ourida <20206209@etud.univ-evry.fr>
2023-07-25 11:25:07 +0200

    all categories with adaptator

--- adaptateur-cable-alimentation/adaptateur-cable-alimentation-data.js
+++ adaptateur-cable-alimentation/adaptateur-cable-alimentation-data.js
@@ -1,16 +1,10 @@
 showDataWithUsbType(
     [
-        {"urlImg":"https://content.grp.collab.group.safran/snm/dsi/pub/CS/SitePages/Lounge_IT/ACCESSOIRES/CDP2VGA.jpg",
-        "nom":"VGA > USB C" ,
-         "description":"Converti VGA en USB C",
-         "ref":"CDP2VGA", 
-         "urlImgCable":"https://content.grp.collab.group.safran/snm/dsi/pub/CS/SitePages/Lounge_IT/ACCESSOIRES/istockphoto-1138477098-612x612-10-3@2x.png",
-         "typeCable":"USB C"
-         },
-         {"urlImg":"https://content.grp.collab.group.safran/snm/dsi/pub/CS/SitePages/Lounge_IT/ACCESSOIRES/CDP2VGA.jpg",
-         "nom":"VGA > USB C" ,
-          "description":"Converti VGA en USB C",
-          "ref":"CDP2VGA", 
+       
+         {"urlImg":"https://content.grp.collab.group.safran/snm/dsi/pub/CS/SitePages/Lounge_IT/ACCESSOIRES/cdp2hd-10-3@2x.png",
+         "nom":"HDMI > USB C" ,
+          "description":"Converti HDMI en USB C",
+          "ref":"CDP2HD", 
           "urlImgCable":"https://content.grp.collab.group.safran/snm/dsi/pub/CS/SitePages/Lounge_IT/ACCESSOIRES/istockphoto-1138477098-612x612-10-3@2x.png",
           "typeCable":"USB C"
           },
@@ -21,9 +15,12 @@ showDataWithUsbType(
            "urlImgCable":"https://content.grp.collab.group.safran/snm/dsi/pub/CS/SitePages/Lounge_IT/ACCESSOIRES/istockphoto-1138477098-612x612-10-3@2x.png",
            "typeCable":"USB C"
            },
-     
+          {"urlImg":"https://content.grp.collab.group.safran/snm/dsi/pub/CS/SitePages/Lounge_IT/ACCESSOIRES/HDE003MB.jpg",
+          "nom":"Câble HDMI" ,
+           "description":"Câble vidéo HDMI de 3cm",
+           "ref":"HDE003MB", 
+           "urlImgCable":"https://content.grp.collab.group.safran/snm/dsi/pub/CS/SitePages/Lounge_IT/ACCESSOIRES/hdmi-icon-nobg.png",
+           "typeCable":"HDMI"
+           },
     ]
- 
-
-    
     , "div0"); 
